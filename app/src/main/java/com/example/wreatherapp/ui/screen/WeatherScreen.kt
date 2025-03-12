4928ff5f2685ebcd04ba63714e806280755eebe8
PC <ledung2020604439@gmail.com>
2025-03-13 00:28:52 +0700

    add AirQuality and WeeklyForecast

--- app/src/main/java/com/example/wreatherapp/ui/screen/WeatherScreen.kt
+++ app/src/main/java/com/example/wreatherapp/ui/screen/WeatherScreen.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.wreatherapp.ui.screen.components.ActionBar
+import com.example.wreatherapp.ui.screen.components.AirQuality
 import com.example.wreatherapp.ui.screen.components.DailyForecast
+import com.example.wreatherapp.ui.screen.components.WeeklyForecast
 import com.example.wreatherapp.ui.theme.ColorBackground
 
 @Composable
@@ -24,6 +28,7 @@ fun WeatherScreen() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddings)
+                .verticalScroll(rememberScrollState())
                 .padding(
                     horizontal = 24.dp,
                     vertical = 10.dp
@@ -34,6 +39,14 @@ fun WeatherScreen() {
                 modifier = Modifier.height(12.dp)
             )
             DailyForecast()
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+            AirQuality()
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+            WeeklyForecast()
         }
 
 
